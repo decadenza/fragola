fc623623548c35a1bfdb7dea273a537b7e122c35
decadenza <30215028+decadenza@users.noreply.github.com>
2018-06-09 15:20:24 +0200

    Fixed CURPATH

--- fragola.sh
+++ fragola.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-CURPATH="`dirname \"$0\"`"
+WORK=1800
+PAUSE=600
+CURPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "$CURPATH/media/icon.ico"
 echo $(date +"%T") "Fragola started"
-notify-send -i $CURPATH/media/icon.ico "Start to work" "It's time to work. Any distraction is forbidden!" && paplay $CURPATH/media/start.ogg
+notify-send -i "$CURPATH/media/icon.ico" "Start to work" "It's time to work. Any distraction is forbidden!" && paplay "$CURPATH/media/start.ogg"
 while true; do
-	sleep 1500 && notify-send -i $CURPATH/media/icon.ico "Time of a break!" "Give yourself a short break!" && echo $(date +"%T") "Break" && paplay $CURPATH/media/break.ogg
-	sleep 300 && notify-send -i $CURPATH/media/icon.ico  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay $CURPATH/media/start.ogg
-	sleep 1500 && notify-send -i $CURPATH/media/icon.ico "Time of a break!" "Give yourself a short break!" && echo $(date +"%T") "Break" && paplay $CURPATH/media/break.ogg
-	sleep 300 && notify-send -i $CURPATH/media/icon.ico  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay $CURPATH/media/start.ogg
-	sleep 1500 && notify-send -i $CURPATH/media/icon.ico "Time of a break!" "Give yourself a short break!" && echo $(date +"%T") "Break" && paplay $CURPATH/media/break.ogg
-	sleep 300 && notify-send -i $CURPATH/media/icon.ico  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay $CURPATH/media/start.ogg
-	sleep 1500 && notify-send -i $CURPATH/media/icon.ico "Time of a LONG break!" "Stand up and give yourself a long break" && echo $(date +"%T") "Long break" && paplay $CURPATH/media/longbreak.ogg
-	sleep 900 && notify-send -i $CURPATH/media/icon.ico  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay $CURPATH/media/start.ogg
+    sleep $WORK && notify-send -i "$CURPATH/media/icon.ico" "Time of a break!" "Give yourself a short break!" && echo $(date +"%T") "Break" && paplay "$CURPATH/media/break.ogg"
+    sleep $PAUSE && notify-send -i "$CURPATH/media/icon.ico"  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay "$CURPATH/media/start.ogg"
+    sleep $WORK && notify-send -i "$CURPATH/media/icon.ico" "Time of a break!" "Give yourself a short break!" && echo $(date +"%T") "Break" && paplay "$CURPATH/media/break.ogg"
+    sleep $PAUSE && notify-send -i "$CURPATH/media/icon.ico"  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay "$CURPATH/media/start.ogg"
+    sleep $WORK && notify-send -i "$CURPATH/media/icon.ico" "Time of a break!" "Give yourself a short break!" && echo $(date +"%T") "Break" && paplay "$CURPATH/media/break.ogg"
+    sleep $PAUSE && notify-send -i "$CURPATH/media/icon.ico"  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay "$CURPATH/media/start.ogg"
+    sleep $WORK && notify-send -i "$CURPATH/media/icon.ico" "Time of a LONG break!" "Stand up and give yourself a long break" && echo $(date +"%T") "Long break" && paplay "$CURPATH/media/longbreak.ogg"
+    sleep $PAUSE*2 && notify-send -i "$CURPATH/media/icon.ico"  "Back to work!" "It's time to work. Any distraction is forbidden!"&& echo $(date +"%T") "Back to work" && paplay "$CURPATH/media/start.ogg"
 done
